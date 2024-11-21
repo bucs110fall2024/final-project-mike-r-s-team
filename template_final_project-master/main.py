14a2ae6215a752ab597fa9d91ccafe65855a42db
Michael-Rullo <michaelrullo2006@gmail.com>
2024-11-20 22:22:24 -0500

    update

--- template_final_project-master/main.py
+++ template_final_project-master/main.py
@@ -1,12 +1,12 @@
 import pygame
-#import your controller
+from src.controller import Controlller
+
 
 def main():
     pygame.init()
-    #Create an instance on your controller object
-    #Call your mainloop
+    controller = Controller()
+    controller.mainloop()
     
-    ###### NOTHING ELSE SHOULD GO IN main(), JUST THE ABOVE 3 LINES OF CODE ######
 
 # https://codefather.tech/blog/if-name-main-python/
 if __name__ == '__main__':
---
+++ template_final_project-master/src/player.py
@@ -0,0 +1,10 @@
+from src.controller import Controller
+
+class Player(pygame, sprite, Sprite):
+    super().__init__()
+
+    def __init__(self):
+        pass
+    
+    def jump(self):
+        pass
