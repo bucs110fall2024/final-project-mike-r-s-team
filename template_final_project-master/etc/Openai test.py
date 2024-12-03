5f4877420466bf61ab12b2ca7419e4e15f5cfa40
Michael-Rullo <michaelrullo2006@gmail.com>
2024-12-03 11:09:01 -0500

    update

--- template_final_project-master/etc/Openai test.py
+++ template_final_project-master/etc/Openai test.py
@@ -8,6 +8,10 @@ load_dotenv(dotenv_path)
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 client = OpenAI(api_key= OPENAI_API_KEY)
+client.files.create(
+  file=open("template_final_project-master\src\ADHD_training_set.jsonl", "rb"),
+  purpose="fine-tune"
+)
 
 completion = client.chat.completions.create(
     model="gpt-4o-mini",
@@ -15,7 +19,7 @@ completion = client.chat.completions.create(
         {"role": "system", "content": "You are a doctor-like bot that will use your knowledge of ADHD to help a patient who is communicating with you as best as you can."},
         {
             "role": "user",
-            "content": "What are 3 symptoms of ADHD. Output your response without any markdown language."
+            "content": "Did all 10 training prompts sucessfully go through?"
         }
     ]
 )
