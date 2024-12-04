5997b9034b2aefdf9701042eb367d1f242fb96f7
Michael-Rullo <michaelrullo2006@gmail.com>
2024-12-03 23:56:45 -0500

    update

--- template_final_project-master/src/chatbot.py
+++ template_final_project-master/src/chatbot.py
@@ -3,8 +3,8 @@ from openai import OpenAI
 class API:
     
     def __init__(self, api_key):
-        client = OpenAI(api_key= api_key)
-        client.files.create(
+        self.client = OpenAI(api_key= api_key)
+        self.client.files.create(
         file=open(r"template_final_project-master\src\ADHD_training_set.jsonl", "rb"),
         purpose="fine-tune"
         )
@@ -18,10 +18,22 @@ class API:
         self.chat_history = history_file.read()
         history_file.close()
     
-    def save(self, messages):
+    def save(self):
         history_file = open(r"template_final_project-master\src\chat_history.jsonl", 'a')
-        history_file.append(messages)
+        history_file.append(self.messages)
         history_file.close()
     
     def uinp(self):
-        pass
+        self.user_input
+    
+    def prompt(self):
+        self.completion = self.client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=[
+                {"role": "system", "content": "You are a doctor-like bot that will use your knowledge of ADHD to help a patient who is communicating with you as best as you can."},
+                {
+                    "role": "user",
+                    "content": "Did all 10 training prompts sucessfully go through?"
+                }
+            ]
+        )
