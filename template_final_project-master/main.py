3279dca9773164c22e6fd4ca78819f10876bb347
Michael-Rullo <michaelrullo2006@gmail.com>
2024-12-05 02:35:41 -0500

    update

--- template_final_project-master/main.py
+++ template_final_project-master/main.py
@@ -1,13 +1,9 @@
-import pygame
 from src.controller import Controller
 
-
 def main():
-    pygame.init()
+    
     controller = Controller()
     controller.mainloop()
-    
 
-# https://codefather.tech/blog/if-name-main-python/
 if __name__ == '__main__':
     main()
--- template_final_project-master/src/chatbot.py
+++ template_final_project-master/src/chatbot.py
@@ -6,7 +6,7 @@ class Api:
         
         self.client = OpenAI(api_key= api_key)
         self.client.files.create(
-        file=open("template_final_project-master\src\ADHD_training_set.jsonl", "rb"),
+        file=open(r"template_final_project-master\src\ADHD_training_set.jsonl", "rb"),
         purpose= "fine-tune"
         )
     
